573033054b66d64534d0854338e7bcedf975fb00
Akash <akashdandur@gmail.com>
2024-02-15 15:06:14 +0530

    Episode -02 Igniting the App

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World!"
-);
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello World! 🚀");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
